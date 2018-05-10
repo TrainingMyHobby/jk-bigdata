b83804835aa3b41732f87a20971cd832c5a6afe9
Kishore Veleti <training.my.hobby@gmail.com>
2018-05-10 02:40:04 -0400

    Initial version of building Datalake and EDW using Adventure works database data on Hadoop environment with Hive, Pig, Spark tech stack

--- adv-works-dl-edw/adv-works-app/config/adv-works-app/ingest/src2lnd/src2lnd_ingest_master_tbls.sh
+++ adv-works-dl-edw/adv-works-app/config/adv-works-app/ingest/src2lnd/src2lnd_ingest_master_tbls.sh
@@ -2,4 +2,7 @@
 
 CURRENT_DIR_PATH="$(pwd -P)"
 
-$JAVA_CMD_HOME/java -cp $ADV_WORKS_APP_LIBS_PATH/*:$ADV_WORKS_APP_CONFIG_PATH jk.bd.app.ingest.Src2LndCLI $CURRENT_DIR_PATH/ingest-tables-config-1.properties
+. "${CURRENT_DIR_PATH}/../../application.conf"
+
+"$CURRENT_DIR_PATH/src2lnd_ingest_common.sh" "$CURRENT_DIR_PATH/src2lnd-master-tables-conf.properties" "$CURRENT_DIR_PATH/src2lnd-master-tables-conf.properties"
+
